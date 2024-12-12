8d6ac66738f7259e267ce10d5ebcdcf1e7bdfe87
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2024-12-12 13:17:31 +0600

    Initial Commit

---
+++ LightSwitchView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LightSwitchView
+//
+//  Created by Shishir_Mac on 12/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LightSwitchView/LightSwitchViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightSwitchViewApp.swift
+//  LightSwitchView
+//
+//  Created by Shishir_Mac on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LightSwitchViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
